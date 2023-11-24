9b6b0e3960c013f1f5ee9e7500383b0a5d5be8c9
asphxx <azikk69@gmail.com>
2023-11-25 01:59:09 +0600

    started working with architechture

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
